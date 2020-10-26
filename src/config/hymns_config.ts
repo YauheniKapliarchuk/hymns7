ec7c1d31268aab91c4017b43eb617f5e5c4c105f
YauheniKapliarchuk <Yauheni.Kapliarchuk@gmail.com>
2020-10-26 21:07:22 +0300

    [DEV] Add new hymn of hope notes 2.

--- src/config/hymns_config.ts
+++ src/config/hymns_config.ts
@@ -1,7 +1,8 @@
 const HYMNS_CONFIG = {
     hymns_of_hope_1997_notes: {
         // 1. Коль Славен
-        1: 'https://i.postimg.cc/PJ1nTTGx/notes-hymn-of-hope-1997-1.jpg'
+        1: 'https://i.postimg.cc/PJ1nTTGx/notes-hymn-of-hope-1997-1.jpg',
+        2: 'https://i.postimg.cc/HnJZMtyx/notes-hymn-of-hope-1997-2.jpg'
     },
 
     hymns_of_hope_1997_text: {
